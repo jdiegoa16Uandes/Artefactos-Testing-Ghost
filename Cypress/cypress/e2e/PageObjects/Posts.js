02a29eb9197cc66351898f30ad5731dfde997b29
jdiego16 <diego8184@hotmail.es>
2024-05-04 16:48:39 -0500

    EP01 EP02 EP03 EP04

--- Cypress/cypress/e2e/PageObjects/Posts.js
+++ Cypress/cypress/e2e/PageObjects/Posts.js
@@ -7,11 +7,11 @@ class PostsPageObject {
     }
 
     async setPostTitle(title) {
-        cy.get('[placeholder="Post title"]').type(title);
+        await cy.get('[placeholder="Post title"]').type(title);
     }
 
     async setPostContent(content) {
-        cy.get('p[data-koenig-dnd-droppable]').type(content).wait(10000);
+        cy.get('p[data-koenig-dnd-droppable]]').type(content).wait(10000);
     }
     async gotoPublish(){
         await cy.get('button[data-test-button="publish-flow"]').then((element) => {
@@ -28,22 +28,44 @@ class PostsPageObject {
             element.click();
         }).wait(2000);
     }
-    async selectType(){
+    async selectType(id){
         await cy.get('p[data-koenig-dnd-droppable]').click().wait(2000);
         await cy.get('div[data-kg-plus-button]').click().wait(2000);
-        await cy.get('button[data-kg-cardmenu-idx="1"]').click().wait(2000);
+        await cy.get(`button[data-kg-cardmenu-idx="${id}"]`).click().wait(2000);
     }
     async setPostContentEspecial(content) {
         await cy.get("div.not-kg-prose").type(content);
         await cy.get('[placeholder="Post title"]').click().wait(2000);
     }
-
-
+    async gotoImage(){
+        await cy.get('button.gh-editor-feature-image-unsplash').then((element) => {
+            element.click();
+        }).wait(2000);
+    }
+    async setImage(search) {
+        cy.get('input.gh-unsplash-search').type(search);
+    }
     validatePost(title) {
         cy.get('h3.gh-content-entry-title').contains(title).should('exist');
     }
+    async selectImage(){
+        await cy.get('div.gh-unsplash-photo-container').first().then(($img) => {
+            cy.wrap($img).find('a.gh-unsplash-button:contains("Insert image")').click();;
+        }).wait(2000);
+    
+    }
     validatePostContent(content){
-        cy.get('div.koenig-react-editor').contains(content).should('exist');
+        const element = cy.get('div.whitespace-normal p')
+        element.contains(content).should('exist')
+    }
+    validatePostContentHTML(content){
+        const element = cy.get('div.min-h-\\[3\\.5vh\\].whitespace-normal h5')
+        element.contains(content).should('exist')
+    }
+    validateImage(){
+        cy.get('div.gh-editor-feature-image').first().then(($div) => {
+            cy.wrap($div).find('img').should('exist');
+        });
     }
     
 }
--- Cypress/cypress/e2e/funcionalidad_01.cy.js
+++ Cypress/cypress/e2e/funcionalidad_01.cy.js
@@ -10,36 +10,36 @@ describe('template spec', () => {
     cy.on('uncaught:exception', (err, runnable) => { console.log(err); return false; });
   });
 
-  // it('EP01 - Crear y publicar post', async () => {
+  it('EP01 - Crear y publicar post', async () => {
   
-  //   const post={
-  //     'title': faker.lorem.word(),
-  //     'content': faker.lorem.paragraphs()
-  //   }
-  //   // Given
-  //   Login.gotoLogin();  
-  //   Login.setLoginEmail();
-  //   Login.setLoginPassword();
-  //   Login.submitLogin();
-  //   await Dashboard.gotoPosts();
-  //   await PostsPageObject.gotoCreatePost();
-  //   // When
-  //   await PostsPageObject.setPostTitle(post.title);
-  //   await PostsPageObject.setPostContent(post.content);
-  //   await PostsPageObject.gotoPublish();
-  //   await PostsPageObject.gotoContinuePublish();
-  //   await PostsPageObject.gotoConfirmPublish();
-  //   await Dashboard.home();
-  //   await Dashboard.gotoPostPublished();
-
-  //   // Then
-  //   PostsPageObject.validatePost(post.title);
-
-  // });
-  it('EP02- Crear y publicar post con contenido markdown', async () => {
     const post={
       'title': faker.lorem.word(),
       'content': faker.lorem.paragraphs()
+    }
+    // Given
+    Login.gotoLogin(); 
+    Login.setLoginEmail();
+    Login.setLoginPassword();
+    Login.submitLogin();
+    await Dashboard.gotoPosts();
+    await PostsPageObject.gotoCreatePost();
+    // When
+    await PostsPageObject.setPostTitle(post.title);
+    await PostsPageObject.setPostContent(post.content);
+    await PostsPageObject.gotoPublish();
+    await PostsPageObject.gotoContinuePublish();
+    await PostsPageObject.gotoConfirmPublish();
+    await Dashboard.home();
+    await Dashboard.gotoPostPublished();
+
+    // Then
+    PostsPageObject.validatePost(post.title);
+
+  });
+  it('EP02- Crear y publicar post con contenido markdown', async () => {
+    const post={
+      'title': faker.lorem.word(),
+      'content': faker.lorem.sentence(),
     }
     // Given
     Login.gotoLogin();  
@@ -50,7 +50,7 @@ describe('template spec', () => {
     await PostsPageObject.gotoCreatePost();
     // When
     await PostsPageObject.setPostTitle(post.title);
-    await PostsPageObject.selectType();
+    await PostsPageObject.selectType("1");
     await PostsPageObject.setPostContentEspecial(post.content);
     await PostsPageObject.gotoPublish();
     await PostsPageObject.gotoContinuePublish();
@@ -63,7 +63,92 @@ describe('template spec', () => {
     PostsPageObject.validatePostContent(post.content);
 
   });
- 
+
+    it('EP03- Crear y publicar post con contenido html', async () => {
+      const post={
+        'title': faker.lorem.word(),
+        'content': faker.lorem.word(),
+      }
+      // Given
+      Login.gotoLogin();  
+      Login.setLoginEmail();
+      Login.setLoginPassword();
+      Login.submitLogin();
+      await Dashboard.gotoPosts();
+      await PostsPageObject.gotoCreatePost();
+      // When
+      await PostsPageObject.setPostTitle(post.title);
+      await PostsPageObject.selectType(2);
+      await PostsPageObject.setPostContentEspecial(`<h5>${post.content}</h5>`);
+      await PostsPageObject.gotoPublish();
+      await PostsPageObject.gotoContinuePublish();
+      await PostsPageObject.gotoConfirmPublish();
+      await Dashboard.home();
+      await Dashboard.gotoPostPublished();
+      await Dashboard.gotoFirstPostPublished(post.title);
+
+      // Then
+      PostsPageObject.validatePostContentHTML(post.content);
+
+    });
+      it('EP04 Crear y publicar post sin titulo', async () => {
+  
+    const post={
+      'title': faker.lorem.word(),
+      'content': faker.lorem.paragraphs()
+    }
+    // Given
+    Login.gotoLogin(); 
+    Login.setLoginEmail();
+    Login.setLoginPassword();
+    Login.submitLogin();
+    await Dashboard.gotoPosts();
+    await PostsPageObject.gotoCreatePost();
+    // When
+    await PostsPageObject.setPostContent(post.content);
+    await PostsPageObject.gotoPublish();
+    await PostsPageObject.gotoContinuePublish();
+    await PostsPageObject.gotoConfirmPublish();
+    await Dashboard.home();
+    await Dashboard.gotoPostPublished();
+
+    // Then
+    PostsPageObject.validatePost("(Untitled)");
+
+  });
+  it('EP04 Crear y publicar post sin titulo', async () => {
+    const post={
+      'title': faker.lorem.word(),
+      'content': faker.lorem.paragraphs()
+    }
+    // Given
+    Login.gotoLogin(); 
+    Login.setLoginEmail();
+    Login.setLoginPassword();
+    Login.submitLogin();
+    await Dashboard.gotoPosts();
+    await PostsPageObject.gotoCreatePost();
+
+
+
+    // When
+    await PostsPageObject.setPostTitle(post.title);
+    await PostsPageObject.gotoImage();
+    await PostsPageObject.setImage("cat");
+    await PostsPageObject.selectImage();
+    await PostsPageObject.gotoPublish();
+    await PostsPageObject.gotoContinuePublish();
+    await PostsPageObject.gotoConfirmPublish();
+    await Dashboard.home();
+    await Dashboard.gotoPostPublished();
+    await Dashboard.gotoFirstPostPublished(post.title);
+
+    // // Then
+    PostsPageObject.validateImage();
+
+  });
 
 
 })
+
+

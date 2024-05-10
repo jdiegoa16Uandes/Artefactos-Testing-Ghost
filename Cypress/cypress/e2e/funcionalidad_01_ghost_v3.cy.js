4abdf005cb5e205471376b092b16e5578a6451ff
jdiego16 <diego8184@hotmail.es>
2024-05-09 20:12:40 -0500

    EP01 EP02 EP03 EP04 EP05 screenshots y pruebas EP01, EP04 en ghost v3

---
+++ Cypress/cypress/e2e/funcionalidad_01_ghost_v3.cy.js
@@ -0,0 +1,81 @@
+import { faker } from "@faker-js/faker";
+import Login from './PageObjects/Login';
+import Dashboard from './PageObjects/Dashboard';
+import PostsPageObject from "./PageObjects/Posts";
+import { beforeEach } from 'mocha';
+
+describe('template spec', () => {
+
+  beforeEach(async () => {
+    cy.on('uncaught:exception', (err, runnable) => { console.log(err); return false; });
+  });
+
+  it('EP01 - Crear y publicar post', async () => {
+  
+    const post={
+      'title': faker.lorem.word(),
+      'content': faker.lorem.paragraphs()
+    };
+    cy.viewport(1920, 1080);
+    // Given
+    Login.gotoLogin(3); 
+    Login.setLoginEmail();
+    Login.setLoginPassword();
+    await cy.screenshot('G5/EP1_1_login', { 'overwrite': true });
+    Login.submitLogin(3);
+    await cy.screenshot('G5/EP1_2_dashboard', { 'overwrite': true });
+    await Dashboard.gotoPosts(3);
+    await cy.screenshot('G5/EP1_3_posts', { 'overwrite': true });
+    await PostsPageObject.gotoCreatePost();
+    await cy.screenshot('G5/EP1_4_postsCreate', { 'overwrite': true });
+    // When
+    await PostsPageObject.setPostTitle(post.title,3);
+    await PostsPageObject.setPostContent(post.content, 3);
+    await cy.screenshot('G5/EP1_5_postsData', { 'overwrite': true });
+    await PostsPageObject.gotoPublish(3);
+    await PostsPageObject.gotoConfirmPublish(3);
+    await Dashboard.home(3);
+    await Dashboard.gotoPostPublished();
+    await cy.screenshot('G5/EP1_6_postsPublished', { 'overwrite': true });
+
+    // Then
+    PostsPageObject.validatePost(post.title);
+
+  });
+
+    it('EP04 Crear y publicar post sin titulo', async () => {
+  
+    const post={
+      'title': faker.lorem.word(),
+      'content': faker.lorem.paragraphs()
+    };
+    cy.viewport(1920, 1080);
+    // Given
+    Login.gotoLogin(3); 
+    Login.setLoginEmail();
+    Login.setLoginPassword();
+    await cy.screenshot('G5/EP4_1_login', { 'overwrite': true });
+    Login.submitLogin(3);
+    await cy.screenshot('G5/EP4_2_dashbboard', { 'overwrite': true });
+    await Dashboard.gotoPosts(3);
+    await cy.screenshot('G5/EP4_3_posts', { 'overwrite': true });
+    await PostsPageObject.gotoCreatePost();
+    await cy.screenshot('G5/EP4_4_postsCreate', { 'overwrite': true });
+    // When
+    await PostsPageObject.setPostContent(post.content, 3);
+    await cy.screenshot('G5/EP4_5_postsData', { 'overwrite': true });
+    await PostsPageObject.gotoPublish(3);
+    await PostsPageObject.gotoConfirmPublish(3);
+    await Dashboard.home(3);
+    await Dashboard.gotoPostPublished();
+    await cy.screenshot('G5/EP4_6_postsPublished', { 'overwrite': true });
+
+
+    // Then
+    PostsPageObject.validatePost("(Untitled)");
+
+  });
+
+})
+
+
--- Cypress/cypress/e2e/funcionalidad_01_ghost_v5.cy.js
+++ Cypress/cypress/e2e/funcionalidad_01_ghost_v5.cy.js
@@ -16,21 +16,28 @@ describe('template spec', () => {
       'title': faker.lorem.word(),
       'content': faker.lorem.paragraphs()
     }
+    cy.viewport(1920, 1080);
     // Given
     Login.gotoLogin(); 
     Login.setLoginEmail();
     Login.setLoginPassword();
+    await cy.screenshot('G3/EP1_1_login', { 'overwrite': true });
     Login.submitLogin();
+    await cy.screenshot('G3/EP1_2_dashboard', { 'overwrite': true });
     await Dashboard.gotoPosts();
+    await cy.screenshot('G3/EP1_3_posts', { 'overwrite': true });
     await PostsPageObject.gotoCreatePost();
+    await cy.screenshot('G3/EP1_4_postsCreate', { 'overwrite': true });
     // When
     await PostsPageObject.setPostTitle(post.title);
     await PostsPageObject.setPostContent(post.content);
+    await cy.screenshot('G3/EP1_5_postsData', { 'overwrite': true });
     await PostsPageObject.gotoPublish();
     await PostsPageObject.gotoContinuePublish();
     await PostsPageObject.gotoConfirmPublish();
     await Dashboard.home();
     await Dashboard.gotoPostPublished();
+    await cy.screenshot('G3/EP1_6_postsPublished', { 'overwrite': true });
 
     // Then
     PostsPageObject.validatePost(post.title);
@@ -40,23 +47,32 @@ describe('template spec', () => {
     const post={
       'title': faker.lorem.word(),
       'content': faker.lorem.sentence(),
-    }
+    };
+    cy.viewport(1920, 1080);
     // Given
     Login.gotoLogin();  
     Login.setLoginEmail();
     Login.setLoginPassword();
+    await cy.screenshot('G3/EP2_1_login', { 'overwrite': true });
     Login.submitLogin();
+    await cy.screenshot('G3/EP2_2_dashbboard', { 'overwrite': true });
     await Dashboard.gotoPosts();
+    await cy.screenshot('G3/EP2_3_posts', { 'overwrite': true });
+
     await PostsPageObject.gotoCreatePost();
+    await cy.screenshot('G3/EP2_4_postsCreate', { 'overwrite': true });
+
     // When
     await PostsPageObject.setPostTitle(post.title);
     await PostsPageObject.selectType("1");
     await PostsPageObject.setPostContentEspecial(post.content);
+    await cy.screenshot('G3/EP2_5_postsData', { 'overwrite': true });
     await PostsPageObject.gotoPublish();
     await PostsPageObject.gotoContinuePublish();
     await PostsPageObject.gotoConfirmPublish();
     await Dashboard.home();
     await Dashboard.gotoPostPublished();
+    await cy.screenshot('G3/EP2_6_postsPublished', { 'overwrite': true });
     await Dashboard.gotoFirstPostPublished(post.title);
 
     // Then
@@ -68,23 +84,31 @@ describe('template spec', () => {
       const post={
         'title': faker.lorem.word(),
         'content': faker.lorem.word(),
-      }
+      };
+      cy.viewport(1920, 1080);
       // Given
       Login.gotoLogin();  
       Login.setLoginEmail();
       Login.setLoginPassword();
+      await cy.screenshot('G3/EP3_1_login', { 'overwrite': true });
       Login.submitLogin();
+      await cy.screenshot('G3/EP3_2_dashbboard', { 'overwrite': true });
       await Dashboard.gotoPosts();
+      await cy.screenshot('G3/EP3_3_posts', { 'overwrite': true });
       await PostsPageObject.gotoCreatePost();
+      await cy.screenshot('G3/EP3_4_postsCreate', { 'overwrite': true });
+
       // When
       await PostsPageObject.setPostTitle(post.title);
       await PostsPageObject.selectType(2);
       await PostsPageObject.setPostContentEspecial(`<h5>${post.content}</h5>`);
+      await cy.screenshot('G3/EP3_5_postsData', { 'overwrite': true });
       await PostsPageObject.gotoPublish();
       await PostsPageObject.gotoContinuePublish();
       await PostsPageObject.gotoConfirmPublish();
       await Dashboard.home();
       await Dashboard.gotoPostPublished();
+      await cy.screenshot('G3/EP3_6_postsPublished', { 'overwrite': true });
       await Dashboard.gotoFirstPostPublished(post.title);
 
       // Then
@@ -96,21 +120,29 @@ describe('template spec', () => {
     const post={
       'title': faker.lorem.word(),
       'content': faker.lorem.paragraphs()
-    }
+    };
+    cy.viewport(1920, 1080);
     // Given
     Login.gotoLogin(); 
     Login.setLoginEmail();
     Login.setLoginPassword();
+    await cy.screenshot('G3/EP4_1_login', { 'overwrite': true });
     Login.submitLogin();
+    await cy.screenshot('G3/EP4_2_dashbboard', { 'overwrite': true });
     await Dashboard.gotoPosts();
+    await cy.screenshot('G3/EP4_3_posts', { 'overwrite': true });
     await PostsPageObject.gotoCreatePost();
+    await cy.screenshot('G3/EP4_4_postsCreate', { 'overwrite': true });
     // When
     await PostsPageObject.setPostContent(post.content);
+    await cy.screenshot('G3/EP4_5_postsData', { 'overwrite': true });
     await PostsPageObject.gotoPublish();
     await PostsPageObject.gotoContinuePublish();
     await PostsPageObject.gotoConfirmPublish();
     await Dashboard.home();
     await Dashboard.gotoPostPublished();
+    await cy.screenshot('G3/EP4_6_postsPublished', { 'overwrite': true });
+
 
     // Then
     PostsPageObject.validatePost("(Untitled)");
@@ -120,25 +152,32 @@ describe('template spec', () => {
     const post={
       'title': faker.lorem.word(),
       'content': faker.lorem.paragraphs()
-    }
+    };
+    cy.viewport(1920, 1080);
     // Given
     Login.gotoLogin(); 
     Login.setLoginEmail();
     Login.setLoginPassword();
+    await cy.screenshot('G3/EP5_1_login', { 'overwrite': true });
     Login.submitLogin();
+    await cy.screenshot('G3/EP5_2_dashbboard', { 'overwrite': true });
     await Dashboard.gotoPosts();
+    await cy.screenshot('G3/EP5_3_posts', { 'overwrite': true });
     await PostsPageObject.gotoCreatePost();
+    await cy.screenshot('G3/EP5_4_postsCreate', { 'overwrite': true });
 
     // When
     await PostsPageObject.setPostTitle(post.title);
     await PostsPageObject.gotoImage();
     await PostsPageObject.setImage("cat");
     await PostsPageObject.selectImage();
+    await cy.screenshot('G3/EP5_5_postsData', { 'overwrite': true });
     await PostsPageObject.gotoPublish();
     await PostsPageObject.gotoContinuePublish();
     await PostsPageObject.gotoConfirmPublish();
     await Dashboard.home();
     await Dashboard.gotoPostPublished();
+    await cy.screenshot('G3/EP4_6_postsPublished', { 'overwrite': true });
     await Dashboard.gotoFirstPostPublished(post.title);
 
     // // Then
